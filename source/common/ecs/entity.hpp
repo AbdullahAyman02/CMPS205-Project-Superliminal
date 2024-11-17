09857bf2e0fed94b3a6f72128cf5d841e9cd71af
Abdullah Ayman <14712020102049@stud.cu.edu.eg>
2024-11-17 19:57:00 +0200

    Implemented requirement 8 successfully

--- source/common/ecs/entity.hpp
+++ source/common/ecs/entity.hpp
@@ -35,19 +35,29 @@ namespace our {
             static_assert(std::is_base_of<Component, T>::value, "T must inherit from Component");
             //TODO: (Req 8) Create an component of type T, set its "owner" to be this entity, then push it into the component's list
             // Don't forget to return a pointer to the new component
-            return nullptr;
+            T* component = new T();
+            component->owner = this;
+            components.push_back(component);
+
+            return component;
         }
 
-        // This template method searhes for a component of type T and returns a pointer to it
+        // This template method searches for a component of type T and returns a pointer to it
         // If no component of type T was found, it returns a nullptr 
         template<typename T>
         T* getComponent(){
             //TODO: (Req 8) Go through the components list and find the first component that can be dynamically cast to "T*".
             // Return the component you found, or return null of nothing was found.
+            for(auto component : components){
+            T* castedComponent = dynamic_cast<T*>(component);
+                if(castedComponent != nullptr){
+                    return castedComponent;
+                }
+            }
             return nullptr;
         }
 
-        // This template method dynami and returns a pointer to it
+        // This template method dynamically casts and returns a pointer to it
         // If no component of type T was found, it returns a nullptr 
         template<typename T>
         T* getComponent(size_t index){
@@ -58,14 +68,22 @@ namespace our {
             return nullptr;
         }
 
-        // This template method searhes for a component of type T and deletes it
+        // This template method searches for a component of type T and deletes it
         template<typename T>
         void deleteComponent(){
             //TODO: (Req 8) Go through the components list and find the first component that can be dynamically cast to "T*".
             // If found, delete the found component and remove it from the components list
+            for(auto it = components.begin(); it != components.end(); ++it){
+                T* castedComponent = dynamic_cast<T*>(*it);
+                if(castedComponent != nullptr){                
+                    delete *it;
+                    components.erase(it);
+                    break;
+                }
+            }
         }
 
-        // This template method searhes for a component of type T and deletes it
+        // This template method searches for a component of type T and deletes it
         void deleteComponent(size_t index){
             auto it = components.begin();
             std::advance(it, index);
@@ -75,16 +93,26 @@ namespace our {
             }
         }
 
-        // This template method searhes for the given component and deletes it
+        // This template method searches for the given component and deletes it
         template<typename T>
         void deleteComponent(T const* component){
             //TODO: (Req 8) Go through the components list and find the given component "component".
             // If found, delete the found component and remove it from the components list
+            for(auto it = components.begin(); it != components.end(); ++it){
+                if(*it == component){
+                    delete *it;
+                    components.erase(it);
+                    break;
+                }
+            }
         }
 
         // Since the entity owns its components, they should be deleted alongside the entity
         ~Entity(){
             //TODO: (Req 8) Delete all the components in "components".
+            for(auto component : components){
+                delete component;
+            }
         }
 
         // Entities should not be copyable
