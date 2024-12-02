bcd2be71ba9ad8b2225c1b639ca23a29565ed6c4
Abdullah Ayman <14712020102049@stud.cu.edu.eg>
2024-12-02 03:15:15 +0200

    Movement bug fixes

--- source/common/systems/free-camera-controller.hpp
+++ source/common/systems/free-camera-controller.hpp
@@ -87,76 +87,94 @@ namespace our
                 float yaw = -delta.x * controller->rotationSensitivity;
 
                 glm::quat yawQuat = glm::angleAxis(yaw, glm::vec3(0, 1, 0));
-                glm::quat pitchQuat = glm::angleAxis(pitch, glm::vec3(1,0,0));
 
-                rotation = yawQuat * rotation * pitchQuat;  // Not sure about this order
+                // To prevent 360 degrees rotation (breaking the neck xD) we limit the pitch to 90 degrees
+                // We also prevent the pitch from exceeding a certain angle from the XZ plane to prevent gimbal locks
+                glm::mat4 M = camera->getOwner()->getLocalToWorldMatrix();
+                // Compute true view matrix
+                glm::vec3 eye = glm::vec3(M * glm::vec4(0, 0, 0, 1));
+                glm::vec3 center = glm::vec3(M * glm::vec4(0, 0, -1, 1));
+                glm::vec3 forward = glm::normalize(center - eye);
+
+                // Check if the forward is above or below the vertical axis, if it is, we reset the pitch to 0 degrees
+                if (glm::dot(forward, glm::vec3(0, 1, 0)) > 0.75f && pitch > 0)
+                    pitch = 0.0f;
+                else if (glm::dot(forward, glm::vec3(0, -1, 0)) > 0.75f && pitch < 0)
+                    pitch = 0.0f;
+
+                glm::quat pitchQuat = glm::angleAxis(pitch, glm::vec3(1, 0, 0));
+
+                rotation = yawQuat * rotation * pitchQuat; // Not sure about this order
                 rotation = glm::normalize(rotation);
             }
 
-        // We prevent the pitch from exceeding a certain angle from the XZ plane to prevent gimbal locks
-        // if (rotation.x < -glm::half_pi<float>() * 0.99f)
-        //     rotation.x = -glm::half_pi<float>() * 0.99f;
-        // if (rotation.x > glm::half_pi<float>() * 0.99f)
-        //     rotation.x = glm::half_pi<float>() * 0.99f;
-        // // This is not necessary, but whenever the rotation goes outside the 0 to 2*PI range, we wrap it back inside.
-        // // This could prevent floating point error if the player rotates in single direction for an extremely long time.
-        // rotation.y = glm::wrapAngle(rotation.y);
+            // We prevent the pitch from exceeding a certain angle from the XZ plane to prevent gimbal locks
+            // if (rotation.x < -glm::half_pi<float>() * 0.99f)
+            //     rotation.x = -glm::half_pi<float>() * 0.99f;
+            // if (rotation.x > glm::half_pi<float>() * 0.99f)
+            //     rotation.x = glm::half_pi<float>() * 0.99f;
+            // // This is not necessary, but whenever the rotation goes outside the 0 to 2*PI range, we wrap it back inside.
+            // // This could prevent floating point error if the player rotates in single direction for an extremely long time.
+            // rotation.y = glm::wrapAngle(rotation.y);
 
-        // We update the camera fov based on the mouse wheel scrolling amount
-        float fov = camera->fovY + app->getMouse().getScrollOffset().y * controller->fovSensitivity;
-        fov = glm::clamp(fov, glm::pi<float>() * 0.01f, glm::pi<float>() * 0.99f); // We keep the fov in the range 0.01*PI to 0.99*PI
-        camera->fovY = fov;
+            // We update the camera fov based on the mouse wheel scrolling amount
+            float fov = camera->fovY + app->getMouse().getScrollOffset().y * controller->fovSensitivity;
+            fov = glm::clamp(fov, glm::pi<float>() * 0.01f, glm::pi<float>() * 0.99f); // We keep the fov in the range 0.01*PI to 0.99*PI
+            camera->fovY = fov;
 
-        // We get the camera model matrix (relative to its parent) to compute the front, up and right directions
-        glm::mat4 matrix = entity->localTransform.toMat4();
+            // We get the camera model matrix (relative to its parent) to compute the front, up and right directions
+            glm::mat4 matrix = entity->localTransform.toMat4();
 
-        // TODO: needs revision
-        glm::vec3 front = glm::normalize(glm::vec3(matrix * glm::vec4(0, 0, -1, 0))),
-                  up = glm::normalize(glm::vec3(matrix * glm::vec4(0, 1, 0, 0))),
-                  right = glm::normalize(glm::vec3(matrix * glm::vec4(1, 0, 0, 0)));
+            // TODO: needs revision
+            glm::vec3 front = glm::normalize(glm::vec3(matrix * glm::vec4(0, 0, -1, 0)));
+            front.y = 0; // We don't want the camera to move up when we move forward, so we project on the xz plane to prevent any slope differences.
+            front = glm::normalize(front);
+            glm::vec3 right = glm::normalize(glm::vec3(matrix * glm::vec4(1, 0, 0, 0)));
+            right.y = 0; // We don't want the camera to move up when we move right, so we project on the xz plane to prevent any slope differences.
+            right = glm::normalize(right);
+            glm::vec3 up = glm::vec3(0, 1, 0); // The up vector is always (0, 1, 0) in world space
 
-        glm::vec3 current_sensitivity = controller->positionSensitivity;
-        // If the LEFT SHIFT key is pressed, we multiply the position sensitivity by the speed up factor
-        if (app->getKeyboard().isPressed(GLFW_KEY_LEFT_SHIFT))
-            current_sensitivity *= controller->speedupFactor;
+            glm::vec3 current_sensitivity = controller->positionSensitivity;
+            // If the LEFT SHIFT key is pressed, we multiply the position sensitivity by the speed up factor
+            if (app->getKeyboard().isPressed(GLFW_KEY_LEFT_SHIFT))
+                current_sensitivity *= controller->speedupFactor;
 
-        // We change the camera position based on the keys WASD/QE
-        // S & W moves the player back and forth
-        if (app->getKeyboard().isPressed(GLFW_KEY_W))
-            position += front * (deltaTime * current_sensitivity.z);
-        if (app->getKeyboard().isPressed(GLFW_KEY_S))
-            position -= front * (deltaTime * current_sensitivity.z);
-        // Q & E moves the player up and down
-        if (app->getKeyboard().isPressed(GLFW_KEY_Q))
-            position += up * (deltaTime * current_sensitivity.y);
-        if (app->getKeyboard().isPressed(GLFW_KEY_E))
-            position -= up * (deltaTime * current_sensitivity.y);
-        // A & D moves the player left or right
-        if (app->getKeyboard().isPressed(GLFW_KEY_D))
-            position += right * (deltaTime * current_sensitivity.x);
-        if (app->getKeyboard().isPressed(GLFW_KEY_A))
-            position -= right * (deltaTime * current_sensitivity.x);
+            // We change the camera position based on the keys WASD/QE
+            // S & W moves the player back and forth
+            if (app->getKeyboard().isPressed(GLFW_KEY_W))
+                position += front * (deltaTime * current_sensitivity.z);
+            if (app->getKeyboard().isPressed(GLFW_KEY_S))
+                position -= front * (deltaTime * current_sensitivity.z);
+            // Q & E moves the player up and down
+            if (app->getKeyboard().isPressed(GLFW_KEY_Q))
+                position += up * (deltaTime * current_sensitivity.y);
+            if (app->getKeyboard().isPressed(GLFW_KEY_E))
+                position -= up * (deltaTime * current_sensitivity.y);
+            // A & D moves the player left or right
+            if (app->getKeyboard().isPressed(GLFW_KEY_D))
+                position += right * (deltaTime * current_sensitivity.x);
+            if (app->getKeyboard().isPressed(GLFW_KEY_A))
+                position -= right * (deltaTime * current_sensitivity.x);
 
-        // We set the entity's position and rotation to the new values
-        entity->localTransform.setPosition(position);
-        entity->localTransform.setOrientation(r3d::Quaternion(rotation.x, rotation.y, rotation.z, rotation.w));
-        RigidBodyComponent *rigidBody = entity->getComponent<RigidBodyComponent>();
-        if (rigidBody && rigidBody->getRigidBody())
-        {
-            rigidBody->getRigidBody()->setTransform(entity->localTransform.getTransform());
+            // We set the entity's position and rotation to the new values
+            entity->localTransform.setPosition(position);
+            entity->localTransform.setOrientation(r3d::Quaternion(rotation.x, rotation.y, rotation.z, rotation.w));
+            RigidBodyComponent *rigidBody = entity->getComponent<RigidBodyComponent>();
+            if (rigidBody && rigidBody->getRigidBody())
+            {
+                rigidBody->getRigidBody()->setTransform(entity->localTransform.getTransform());
+            }
         }
-    }
 
-    // When the state exits, it should call this function to ensure the mouse is unlocked
-    void
-    exit()
-    {
-        if (mouse_locked)
+        // When the state exits, it should call this function to ensure the mouse is unlocked
+        void
+        exit()
         {
-            mouse_locked = false;
-            app->getMouse().unlockMouse(app->getWindow());
+            if (mouse_locked)
+            {
+                mouse_locked = false;
+                app->getMouse().unlockMouse(app->getWindow());
+            }
         }
-    }
-
-};
+    };
 }
